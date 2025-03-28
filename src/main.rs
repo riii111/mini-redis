602a97fd303004946b56efb83582349e2ca4d7a2
ryo-ichinose <ryo_ichinose@pj100.biz>
2025-03-28 12:35:35 +0900

    typo

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
 async fn main() {
-    let listner = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
     loop {
-        let (socket, _) = listner.accept().await.unwrap();
+        let (socket, _) = listener.accept().await.unwrap();
         tokio::spawn(async move {
             process(socket).await;
         });
