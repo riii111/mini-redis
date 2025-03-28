9cf4d5dd212ffe91d8867857338c8092f1772313
ryo-ichinose <ryo_ichinose@pj100.biz>
2025-03-28 12:44:37 +0900

    feat. store the value

--- src/main.rs
+++ src/main.rs
@@ -14,15 +14,30 @@ async fn main() {
 }
 
 async fn process(socket: TcpStream) {
-    // The 'Connection' lets us read/write redis **frames** instead of byte streams.
-    // The `Connection` type is defined by mini-redis.
+    use mini_redis::Command::{self, Get, Set};
+    use std::collections::HashMap;
+
+    let mut db = HashMap::new();
+
     let mut connection = Connection::new(socket);
 
-    if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("GOT: {:?}", frame);
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    // For now, '&Vec<u8>' is converted to 'Bytes' using 'into()'.
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplemented {:?}", cmd),
+        };
 
-        // Respond with an error
-        let response = Frame::Error("unimplemented".to_string());
         connection.write_frame(&response).await.unwrap();
     }
 }
